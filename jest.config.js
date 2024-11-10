d6e5376d8db02680875396134870dd301f653ccd
DON BADSHA <DON@letsgors.com>
2024-11-09 21:56:36 -0500

    Update jest.config.js
    Signed-off-by: DON BADSHA <DON@letsgors.com>

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+  },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
 };
