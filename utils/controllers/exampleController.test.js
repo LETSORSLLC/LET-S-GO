9484e8995f75f1d780360048c78d3846e80e547e
DON BADSHA <DON@letsgors.com>
2024-11-09 20:33:28 -0500

    Update exampleController.test.js
    Signed-off-by: DON BADSHA <DON@letsgors.com>

--- utils/controllers/exampleController.test.js
+++ utils/controllers/exampleController.test.js
@@ -1,11 +1,11 @@
-const { exampleFunction } = require('./exampleController');
+const { exampleFunction } = require('./exampleController.js');
 const httpMocks = require('node-mocks-http');
 
 test('exampleFunction should send "Hello, world!"', () => {
     const req = httpMocks.createRequest();
     const res = httpMocks.createResponse();
-    
+
     exampleFunction(req, res);
-    
+
     expect(res._getData()).toBe('Hello, world!');
 });
